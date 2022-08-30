c5216809316b6b30247ef40cffee18d17e49cc2e
Pablo Lezcano <pabloromanlezcano@gmail.com>
2022-08-30 20:56:28 -0300

    edit README

--- components/Rings.js
+++ components/Rings.js
@@ -32,22 +32,22 @@ const Rings = () => {
     <div className="landing-header" ref={vantaRef}>
          <div className="header-text">
                 <div className="content">
-                    <h5 className="name text-nowrap ">Pablo Lezcano</h5>
+                    <h2 className="name text-nowrap ">Pablo Lezcano</h2>
                     <span> <h5>Desarrollador JS</h5>
                         <ul className="header-links p-0 d-flex justify-content-between align-items-center">
                           <li>
                             <div class="m-2 card-small-a" >
-                              <img src="github.svg" alt="" width="40%" height="auto"/>
+                              <img src="github.svg" alt="" width="35" height="auto"/>
                             </div>
                           </li>
                           <li>
                             <div class="m-2 card-small-a">
-                              <img src="linkedin.svg" alt="" width="40%" height="auto"/>
+                              <img src="linkedin.svg" alt="" width="35" height="auto"/>
                             </div>
                           </li>
                           <li>
                           <div class="m-2 card-small-a">
-                            <img src="mail4.svg" alt="" width="40%" height="auto"/>
+                            <img src="mail4.svg" alt="" width="35" height="auto"/>
                           </div>
                           </li>
                         </ul>
